0a4acc077a13fbaf953ab9abd0dc77f7044b62c9
crystal1229d <crystal1229d@naver.com>
2021-11-17 23:19:00 +0900

    add products_by_id router for get request from ProductDetailPage

--- client/src/components/views/ProductDetailPage/ProductDetailPage.js
+++ client/src/components/views/ProductDetailPage/ProductDetailPage.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function ProductDetailPage(props) {
+
+    const productId = props.match.params.productId
+
+    useEffect(() => {
+        
+        axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response)
+                } else {
+                    alert('상세 정보 가져오기를 실패했습니다')
+                }
+            })
+
+    }, [])
+
     return (
         <div>
             ProductDetailPage
