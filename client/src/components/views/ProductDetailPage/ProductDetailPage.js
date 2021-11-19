319c829fdc4491e1035b69a0bdc9159eda8512b6
crystal1229d <crystal1229d@naver.com>
2021-11-19 23:23:59 +0900

    add ProductImage with ImageSlider in it

--- client/src/components/views/ProductDetailPage/ProductDetailPage.js
+++ client/src/components/views/ProductDetailPage/ProductDetailPage.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import ProductImage from './Sections/ProductImage'
+import ProductInfo from './Sections/ProductInfo'
+import { Row, Col } from 'antd'
 
 function ProductDetailPage(props) {
 
     const productId = props.match.params.productId
+
+    const [Product, setProduct] = useState({})
 
     useEffect(() => {
         
         axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then(response => {
                 if (response.data.success) {
-                    console.log(response)
+                    setProduct(response.data.product[0])
                 } else {
                     alert('상세 정보 가져오기를 실패했습니다')
                 }
@@ -19,8 +24,25 @@ function ProductDetailPage(props) {
     }, [])
 
     return (
-        <div>
-            ProductDetailPage
+        <div style={{ width:'100%', padding:'3rem 4rem' }}>
+            
+            <div style={{ display:'flex', justifyContent:'center' }}>
+                <h1>{Product.name}</h1>
+            </div>
+            <br />
+
+            <Row gutter={[16, 16]}>
+            
+                <Col lg={12} sm={24}>
+                    <ProductImage detail={Product} />
+                </Col>
+
+                <Col lg={12} sm={24}>
+                    <ProductInfo />
+                </Col>
+            
+            </Row>
+
         </div>
     )
 }
