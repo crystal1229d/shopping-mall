2caaf6d640120f192c44e939bd825a9350ba8b2b
crystal1229d <crystal1229d@naver.com>
2021-12-01 21:57:25 +0900

    add totalsum in CartPage

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems } from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
@@ -6,6 +6,8 @@ import UserCardBlock from './Sections/UserCardBlock';
 function CartPage(props) {
 
     const dispatch = useDispatch();
+
+    const [Total, setTotal] = useState(0)
 
     useEffect(() => {
 
@@ -20,6 +22,7 @@ function CartPage(props) {
 
                 // 원래 axios 를 해야 하는데, 현재 Redux 를 이용하고 있으므로 대신 dispatch 실행
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
+                    .then(response => { calculateTotal(response.payload) })
 
             } else {
 
@@ -28,6 +31,16 @@ function CartPage(props) {
 
     }, [props.user.userData])
 
+    let calculateTotal = (cartDetail) => {
+        let total = 0;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        })
+
+        setTotal(total);
+    }
+
     return (
         <div style={{ width:'85%', margin:'3rem auto' }}>
 
@@ -35,6 +48,10 @@ function CartPage(props) {
 
             <div>
                 <UserCardBlock products={props.user.cartDetail} />
+            </div>
+
+            <div style={{ marginTop:'3rem' }}>
+                <h2>Total Amount: ${Total}</h2>
             </div>
 
         </div>
